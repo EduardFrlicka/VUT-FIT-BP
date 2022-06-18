2221ca7dad4de2a4b2c10830826f84d89ffd03f4
Eduard Frlicka <edo.frlicka@gmail.com>
2022-06-18 10:30:12 +0200

    quickfix missing macro

--- src/messages.h
+++ src/messages.h
@@ -3,7 +3,7 @@
 #define MSG_SCCESS ""
 
 #define MSG_TOKEN_STACK_EMPTY "Emty stack"
-#define MSG_TOKEN_STACK_PTR_NULL "Stack pointer not initilized"
+#define MSG_TOKEN_STACK_PTR_NULL "Stack pointer is NULL"
 
 
 
